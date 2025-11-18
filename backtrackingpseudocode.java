d7a7fdebb4579e93f45bceb094187d2ffce6782c
catmw <157846537+catmw@users.noreply.github.com>
2025-11-18 19:38:03 +0000

    fix indentation

--- backtrackingpseudocode.java
+++ backtrackingpseudocode.java
@@ -7,41 +7,41 @@ visited = 2D array initialised to false
 
 if backtrack(start.row, start.col):
     return true //valid route found
-    else 
+else 
     return false //no valid route
 
-    function backtrack(row, col):
-        if (row, col) doesnt exist in fabric:
-            return false
+function backtrack(row, col):
+    if (row, col) doesnt exist in fabric:
+        return false
 
-        if visited(row, col) is true:
-            return false
+    if visited(row, col) is true:
+        return false
 
-        if (row, col) is 503:
-            return false
+    if (row, col) is 503:
+        return false
 
-        if (row, col) is goal:
+    if (row, col) is goal:
             return true
 
-        //mark the cell as visited
-        mark visited(row, col) true
+    //mark the cell as visited
+    mark visited(row, col) true
 
 
-        
-        //move in the fabric
 
-        //up 
-        if backtrack(row - 1,col):   
-            return true 
-        //down
-        if backtrack(row + 1,col):
-            return true
+//move in the fabric
 
-        //right
-        if backtrack(row, col+1):
-            return true
-        //left
-        if backtrack(row, col- 1):
-            return true
+//up 
+if backtrack(row - 1,col):   
+    return true 
+//down
+if backtrack(row + 1,col):
+    return true
+
+//right
+if backtrack(row, col+1):
+    return true
+//left
+if backtrack(row, col- 1):
+    return true
        
  */
