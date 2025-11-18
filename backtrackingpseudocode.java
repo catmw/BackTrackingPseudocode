04255ab313e1715302722949c343d5aa370bc04b
catmw <157846537+catmw@users.noreply.github.com>
2025-11-18 19:46:00 +0000

    Update backtrackingpseudocode.java

--- backtrackingpseudocode.java
+++ backtrackingpseudocode.java
@@ -1,47 +1,47 @@
 /* 
 function routePacket(fabric, start, goal):
-rows = number of rows in fabric
-cols = number of columns in fabric
+    rows = number of rows in fabric
+    cols = number of columns in fabric
 
-visited = 2D array initialised to false
+    visited = 2D array initialised to false
 
-if backtrack(start.row, start.col):
-    return true //valid route found
-else 
-    return false //no valid route
+    if backtrack(start.row, start.col):
+        return true //valid route found
+    else 
+        return false //no valid route
 
-function backtrack(row, col):
-    if (row, col) doesnt exist in fabric:
-        return false
+    function backtrack(row, col):
+        if (row, col) doesnt exist in fabric:
+            return false
 
-    if visited(row, col) is true:
-        return false
+        if visited(row, col) is true:
+            return false
 
-    if (row, col) is 503:
-        return false
+        if (row, col) is 503:
+            return false
 
-    if (row, col) is goal:
+        if (row, col) is goal:
+                return true
+
+        //mark the cell as visited
+        mark visited(row, col) true
+
+
+
+        //move in the fabric
+
+        //up 
+        if backtrack(row - 1,col):   
+            return true 
+        //down
+        if backtrack(row + 1,col):
             return true
 
-    //mark the cell as visited
-    mark visited(row, col) true
-
-
-
-    //move in the fabric
-
-    //up 
-    if backtrack(row - 1,col):   
-        return true 
-    //down
-    if backtrack(row + 1,col):
-        return true
-
-    //right
-    if backtrack(row, col+1):
-        return true
-    //left
-    if backtrack(row, col- 1):
-        return true
+        //right
+        if backtrack(row, col+1):
+            return true
+        //left
+        if backtrack(row, col- 1):
+            return true
        
  */
